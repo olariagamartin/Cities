cfa0261fc632248849c522d7e5f5f60d16e83bed
martin.olariaga <martin.olariaga@geopagos.com>
2025-07-20 23:06:40 -0300

    Feature: use nav host

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.navigation.compose)
 
     // koin
     implementation(libs.koin.android)
--- app/src/main/java/com/themarto/cities/MainActivity.kt
+++ app/src/main/java/com/themarto/cities/MainActivity.kt
@@ -5,8 +5,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.themarto.cities.ui.theme.CitiesTheme
 import com.themarto.features.cityList.CitiesScreen
 
@@ -17,7 +21,15 @@ class MainActivity : ComponentActivity() {
         setContent {
             CitiesTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    CitiesScreen()
+                    NavHost(
+                        modifier = Modifier.padding(innerPadding),
+                        navController = rememberNavController(),
+                        startDestination = "cities"
+                    ) {
+                        composable("cities") {
+                            CitiesScreen()
+                        }
+                    }
                 }
             }
         }
