61256a6ce6be3611a523a973606c72aabdcf4fd7
martin.olariaga <martin.olariaga@geopagos.com>
2025-07-20 23:09:35 -0300

    Refactor: extract MainAppContainer

--- app/src/main/java/com/themarto/cities/MainActivity.kt
+++ app/src/main/java/com/themarto/cities/MainActivity.kt
@@ -4,34 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.ui.Modifier
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import com.themarto.cities.ui.theme.CitiesTheme
-import com.themarto.features.cityList.CitiesScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            CitiesTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    NavHost(
-                        modifier = Modifier.padding(innerPadding),
-                        navController = rememberNavController(),
-                        startDestination = "cities"
-                    ) {
-                        composable("cities") {
-                            CitiesScreen()
-                        }
-                    }
-                }
-            }
+            MainAppContainer()
         }
     }
 }
---
+++ app/src/main/java/com/themarto/cities/MainAppContainer.kt
@@ -0,0 +1,29 @@
+package com.themarto.cities
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.themarto.cities.ui.theme.CitiesTheme
+import com.themarto.features.cityList.CitiesScreen
+
+@Composable
+fun MainAppContainer(modifier: Modifier = Modifier) {
+    CitiesTheme {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            NavHost(
+                modifier = Modifier.padding(innerPadding),
+                navController = rememberNavController(),
+                startDestination = "cities"
+            ) {
+                composable("cities") {
+                    CitiesScreen()
+                }
+            }
+        }
+    }
+}
