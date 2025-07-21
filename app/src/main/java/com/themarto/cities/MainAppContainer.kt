7f3a5540ecdcf31bc4fd1e9bc91859c98b27f46b
martin.olariaga <martin.olariaga@geopagos.com>
2025-07-21 01:32:10 -0300

    Feature: Implement navigation with NavHost

--- app/src/main/java/com/themarto/cities/MainAppContainer.kt
+++ app/src/main/java/com/themarto/cities/MainAppContainer.kt
@@ -5,23 +5,45 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.themarto.cities.ui.theme.CitiesTheme
 import com.themarto.features.cityList.CitiesScreen
+import com.themarto.features.cityList.MapScreen
 
 @Composable
 fun MainAppContainer() {
     CitiesTheme {
         Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            val navController = rememberNavController()
             NavHost(
                 modifier = Modifier.padding(innerPadding),
-                navController = rememberNavController(),
+                navController = navController,
                 startDestination = "cities"
             ) {
-                composable("cities") {
-                    CitiesScreen()
+                composable(
+                    route = "cities"
+                ) {
+                    CitiesScreen(
+                        onCityClick = {
+                            navController.navigate("map/$it")
+                        }
+                    )
+                }
+
+                composable(
+                    route = "map/{city_id}",
+                    arguments = listOf(
+                        navArgument("city_id") { type = NavType.StringType }
+                    )
+                ) { backStackEntry ->
+                    MapScreen(
+                        onNavigateBack = { navController.popBackStack() },
+                        cityId = backStackEntry.arguments?.getString("city_id")!!
+                    )
                 }
             }
         }
