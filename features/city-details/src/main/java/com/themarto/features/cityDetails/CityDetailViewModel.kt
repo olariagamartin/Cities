1f679fa16f7f5d20763a8eb182d613c530f6cf52
martin.olariaga <martin.olariaga@geopagos.com>
2025-07-22 01:20:58 -0300

    Feature: fetch city data from CityDetailViewModel

--- features/city-details/src/main/java/com/themarto/features/cityDetails/CityDetailViewModel.kt
+++ features/city-details/src/main/java/com/themarto/features/cityDetails/CityDetailViewModel.kt
@@ -1,17 +1,37 @@
 package com.themarto.features.cityDetails
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.themarto.core.data.repository.CityRepository
+import com.themarto.core.data.utils.Result
 import com.themarto.core.domain.City
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 
 data class UiState(
     val city: City? = null
 )
 
-class CityDetailViewModel() : ViewModel() {
+class CityDetailViewModel(
+    private val cityRepository: CityRepository,
+    private val cityId: String
+) : ViewModel() {
 
     private val _uiState = MutableStateFlow(UiState())
     val uiState: StateFlow<UiState> = _uiState.asStateFlow()
+
+    init {
+        loadCity()
+    }
+
+    private fun loadCity() {
+        viewModelScope.launch {
+            val result = cityRepository.getCityById(cityId)
+            if (result is Result.Success) {
+                _uiState.value = _uiState.value.copy(city = result.data)
+            }
+        }
+    }
 }
--- features/city-details/src/test/java/com/themarto/features/cityDetails/CityDetailVMTest.kt
+++ features/city-details/src/test/java/com/themarto/features/cityDetails/CityDetailVMTest.kt
@@ -1,8 +1,14 @@
 package com.themarto.features.cityDetails
 
 import app.cash.turbine.test
+import com.themarto.core.data.repository.CityRepository
+import com.themarto.core.data.utils.Result
+import com.themarto.core.domain.City
+import com.themarto.core.domain.Coordinates
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
@@ -10,7 +16,10 @@ import org.junit.After
 import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
+import org.mockito.Mockito.spy
+import org.mockito.Mockito.verify
 
+@ExperimentalCoroutinesApi
 class CityDetailVMTest {
 
     private val testDispatcher = StandardTestDispatcher()
@@ -27,12 +36,78 @@ class CityDetailVMTest {
 
     @Test
     fun `A0_WHEN ViewModel is initialized THEN city is null`() = runTest {
-        val vm = CityDetailViewModel()
+        val vm = CityDetailViewModel(
+            cityRepository = provideCityRepository(),
+            cityId = "123"
+        )
 
         vm.uiState.test {
             assertEquals(null, awaitItem().city)
             cancelAndIgnoreRemainingEvents()
         }
 
+    }
+
+    @Test
+    fun `B0_WHEN ViewModel is initialized THEN getCityById is called`() = runTest {
+        val repo = spy(provideCityRepository())
+        val cityId = "123"
+
+        val vm = CityDetailViewModel(
+            cityRepository = repo,
+            cityId = cityId
+        )
+
+        advanceUntilIdle()
+
+        verify(repo).getCityById(cityId)
+
+    }
+
+    @Test
+    fun `B1_WHEN getCityById respond success THEN city is displayed`() = runTest {
+        val vm = CityDetailViewModel(
+            cityRepository = provideCityRepository(),
+            cityId = "123"
+        )
+        vm.uiState.test {
+            awaitItem() // initial emit
+            assertEquals(provideCityList().first(), awaitItem().city)
+            cancelAndIgnoreRemainingEvents()
+        }
+    }
+
+    // ----------- helper functions --------------------
+    // duplicated: could be in a separate test module
+    private fun provideCityRepository(
+        getCitiesFiltered: suspend (String, Boolean) -> Result<List<City>> = { prefix, filterFav ->
+            Result.Success(provideCityList()
+                .filter {
+                    it.name.startsWith(prefix).and(!filterFav || it.isFavorite)
+                })
+        }
+    ): CityRepository {
+        return object : CityRepository {
+
+            override suspend fun getCitiesFiltered(prefix: String, filterFav: Boolean): Result<List<City>> {
+                return getCitiesFiltered(prefix, filterFav)
+            }
+
+            override suspend fun toggleFavorite(id: String) {
+                // nothing
+            }
+
+            override suspend fun getCityById(id: String): Result<City> {
+                return Result.Success(provideCityList().first { it.id == id })
+            }
+        }
+    }
+
+    fun provideCityList() : List<City> {
+        return listOf(
+            City("123", "name", "country", Coordinates(1.0, 2.0), false),
+            City("124", "name", "country", Coordinates(1.0, 2.0), false),
+            City("125", "name", "country", Coordinates(1.0, 2.0), false),
+        )
     }
 }
